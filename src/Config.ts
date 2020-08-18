8c687db8fa49da5570f79501981b39bb057d98f1
Leandro Ferreira <leandro.daf4@gmail.com>
2020-08-18 08:53:47 -0300

    Basic application structure

---
+++ src/Config.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export interface Config {
+  port: number;
+  debugLogging: boolean;
+  elasticHost: string;
+}
+
+const isDevMode = process.env.NODE_ENV == "development";
+
+export const config: Config = {
+  port: +(process.env.PORT || 3000),
+  debugLogging: isDevMode,
+  elasticHost: process.env.ELASTIC_HOST,
+};
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import helmet from "koa-helmet";
+import cors from "@koa/cors";
+
+import { config } from "./Config";
+import { unprotectedRouter } from "./UnprotectedRoutes";
+
+const app = new Koa();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(bodyParser());
+
+app.use(unprotectedRouter.routes());
+
+app.listen(config.port);
+
+console.log(`Server running on port ${config.port}`);
